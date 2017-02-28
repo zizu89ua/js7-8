6e1dc4e9a84cd5c962dc0998ea18fed9c5145d4c
Dmytro Kharchenko <dimonkharchenko89@gmail.com>
2017-02-28 22:01:29 +0200

    added second part

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 
 
 $(function (){
-	// var $link = $('a');
+
 	var $li = $('li');
 	$li.css({
 		'list-style': 'none',
@@ -26,21 +26,57 @@ $(function (){
 		'overflow': 'auto',
 		'height': '165px'
 	});
-	$('#wrap__leftab').on('click', function(){
-		$('#left-col').show();
-		$('#center-col').hide();
-		$('#right-col').hide();
-	});
-	$('#wrap__centertab').on('click', function(){
-		$('#center-col').show();
-		$('#left-col').hide();
-		$('#right-col').hide();
-	});
-	$('#wrap__righttab').on('click', function(){
-		$('#right-col').show();
-		$('#left-col').hide();
-		$('#center-col').hide();
+	$('.wrap__link').click(function() {
+            $('.text-block p').removeClass('active');
+            $('#'+ $(this).data('id')).addClass('active');
+        })
+	// $('#wrap__leftab').on('click', function(){
+	// 	$('#left-col').show();
+	// 	$('#center-col').hide();
+	// 	$('#right-col').hide();
+	// });
+	// $('#wrap__centertab').on('click', function(){
+	// 	$('#center-col').show();
+	// 	$('#left-col').hide();
+	// 	$('#right-col').hide();
+	// });
+	// $('#wrap__righttab').on('click', function(){
+	// 	$('#right-col').show();
+	// 	$('#left-col').hide();
+	// 	$('#center-col').hide();
 		
-	})
+	// })
+	$('input').hover(function(){
+		$(this).next('.title').animate({
+			opacity: 'show',
+			left: '50'
+		}, 1000);
 
+	}, function(){
+		$(this).next('.title').animate({
+			opacity: 'hide',
+			left: '60'
+		}, 'fast')
+	})
+	$('.title').css({
+		'background': '#4c79b3',
+		'border': '2px solid #cdc',
+		'border-radius': '5px',
+		'color': '#fff',
+		'font-size': '15px',
+		'padding': '10px 5px',
+	})
+	$('.show-title').on('click', function(){
+		$('.title').show()
+	})
+	$('.show-title').css({
+		'background': '#fff',
+		'border': '1px solid #ccc',
+		'border-radius': '5px',
+		'color': '#454545',
+		'height': '40px',
+		'margin-top': '20px',
+		'outline': 'none',
+		'width': '100px'
+	})
 });
